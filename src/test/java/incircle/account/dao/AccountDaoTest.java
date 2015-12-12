b2a75e140e348a36f6e17828c35d4ad7dbeb410a
Joe <fdu.zhuofan.ma@gmail.com>
2015-12-12 17:02:17 -0500

    Add testUpdateAccount

--- src/test/java/incircle/account/dao/AccountDaoTest.java
+++ src/test/java/incircle/account/dao/AccountDaoTest.java
@@ -8,7 +8,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Date;
+
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 
 /**
  * Created by Julian on 12/10/2015.
@@ -40,5 +43,21 @@ public class AccountDaoTest {
     @Test
     public void testGetAccount() {
         assertNotNull(accountDao.findByAccountName("shabi"));
+    }
+
+    @Test
+    public void testUpdateAccount() {
+        Account account2 = new Account("wocao", "wocao", true);
+        account2.setJumpwill(true);
+        account2.setJumpdate(new Date());
+        Session session = sessionFactory.getCurrentSession();
+        session.beginTransaction();
+        session.save(account2);
+        session.getTransaction().commit();
+        Long id = accountDao.findByAccountName("wocao").getId();
+        Account newAccount = new Account();
+        newAccount.setJumpwill(false);
+        Account account3 = accountDao.updateAccount(newAccount, id);
+        assertNull(account3.getJumpdate());
     }
 }
